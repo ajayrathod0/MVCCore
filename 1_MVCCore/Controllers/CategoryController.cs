73b914ffdf0980e67f68b2d38d626ddc0e2c25ac
Ajay Rathod <128123668+ajayrathod0@users.noreply.github.com>
2024-05-31 15:44:51 +0530

    Add project files.

---
+++ 1_MVCCore/Controllers/CategoryController.cs
@@ -0,0 +1,19 @@
+﻿using _1_MVCCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _1_MVCCore.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Category> categories = new List<Category>() {
+            new Category(){CategoryId =1,CategoryName=" Mens Wear" },
+            new Category(){CategoryId =2,CategoryName = "Women Wear"}
+            };
+            return View(categories);
+        }
+
+     
+    }
+}
---
+++ 1_MVCCore/Models/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace _1_MVCCore.Models
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } 
+    }
+}
